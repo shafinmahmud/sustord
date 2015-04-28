5878204f406d1042f7a6ea8ecac53ddbea5040cc
Shafin Mahmud <shafin.mahmud@gmail.com>
2015-04-28 22:51:01 +0600

    Service inheritance added

---
+++ src/main/java/me/shafin/sustord/service/IdentityService.java
@@ -0,0 +1,31 @@
+/*
+ */
+package me.shafin.sustord.service;
+
+import me.shafin.sustord.dao.StudentInfoDao;
+import me.shafin.sustord.model.StudentInfo;
+import me.shafin.sustord.utility.ServiceDispatcher;
+
+/**
+ *
+ * @author SHAFIN
+ */
+public class IdentityService {
+    public final StudentInfo studentInfo;
+
+    /* Constructor thats is private and get accesses through static helper method  */
+    public IdentityService(StudentInfo studentInfo) throws Exception {
+        this.studentInfo = studentInfo;
+    }
+
+    /* Constructor helper for singleton AcademicInfoService  */
+    public static StudentInfo forSingletonIdentityService(String registrationNo) throws Exception {
+        return ServiceDispatcher.getSingletonStudentInfo(registrationNo);
+    }
+
+    /* Constructor helper for prototype AcademicInfoService  */
+    public static StudentInfo forProtypeIdentityService(String registrationNo) throws Exception {
+        return StudentInfoDao.getStudentInfoObject(registrationNo);
+    }
+    
+}
