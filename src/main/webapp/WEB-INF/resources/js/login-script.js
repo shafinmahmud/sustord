5c08cf2a2da00ee319748fb222b24c8e6f76a473
Shafin Mahmud <shafin.mahmud@gmail.com>
2016-07-09 03:50:42 +0600

    home page template added

--- src/main/webapp/WEB-INF/resources/js/login-script.js
+++ src/main/webapp/WEB-INF/resources/js/login-script.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
 	// --------------- ICON AND ANIMATION URLs -------------------
-	var loaderUrl = "../resources/icons/ajax-loader.gif";
-	var successUrl = "../resources/icons/success-icon.png";
-	var emptyIconUrl = "../resources/icons/empty-icon.gif";
-	
-	$("#loading-anim").attr("src", emptyIconUrl);
-	
+	var loaderUrl = "/sustord/resources/icons/ajax-loader.gif";
+		
 	// Onclick actions for the Login button
 	$('#login-form').submit(function(event) {
 		$("#loading-anim").attr("src", loaderUrl);
 	});
-
 });
