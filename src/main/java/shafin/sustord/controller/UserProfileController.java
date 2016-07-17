fd92b862f2b86b1295d809b832dcb4f154fcb4cc
Shafin Mahmud <shafin.mahmud@gmail.com>
2016-07-17 08:30:12 +0600

    progress

--- src/main/java/shafin/sustord/controller/UserProfileController.java
+++ src/main/java/shafin/sustord/controller/UserProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import shafin.sustord.dto.UserProfileDto;
+import shafin.sustord.pojo.PersonalInfoPojo;
 import shafin.sustord.service.StudentinfoPersonal;
 
 @Controller
@@ -22,12 +23,33 @@ public class UserProfileController {
 		if (loginSession.getId() == null || !loginSession.getRole().equals("student"))
 			return "redirect:/login/user";
 		
-		getProfileDTO();
+		model.addAttribute("profile", getProfileDTO());
 		return "profile";
 	}
 	
 	private UserProfileDto getProfileDTO(){
 		StudentinfoPersonal personalService = new StudentinfoPersonal(loginSession.getId());
-		return null;
+		PersonalInfoPojo pojo = personalService.getStudentPersonalInfo();
+		return wrapPersonalInfoPojoIntoUserProfileDto(pojo);
 	}
+	
+	public static UserProfileDto wrapPersonalInfoPojoIntoUserProfileDto(PersonalInfoPojo pojo){
+		UserProfileDto dto = new UserProfileDto();
+		dto.setName(pojo.getName());
+		dto.setFathersName(pojo.getFathersName());
+		dto.setMothersName(pojo.getMothersName());
+		dto.setBloodGroup(pojo.getBloodGroup());
+		dto.setPhone(pojo.getPhone());
+		dto.setDateOfBirth(pojo.getDateOfBirth());
+		dto.setEmail(pojo.getEmail());
+		dto.setMaritalStatus(pojo.getMaritalStatus());
+		dto.setNationality(pojo.getNationality());
+		dto.setPermanentAddress(pojo.getPermanentAddress());
+		dto.setPhotoUrl(pojo.getPhotoUrl());
+		dto.setPresentAddress(pojo.getPresentAddress());
+		dto.setReligion(pojo.getReligion());
+		dto.setSex(pojo.getSex());
+		return dto;
+	}
+	
 }
